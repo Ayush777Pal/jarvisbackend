719e856789a51c823c05456162f867e5e7213a2f
Ayush Pal <ayush777pal2004@gmail.com>
2026-06-11 23:07:22 +0530

    no need for differnt memory file

--- memory/services.py
+++ memory/services.py
@@ -1,4 +1,16 @@
 from .models import Memory
+import requests
+import json
+import os
+
+OPENROUTER_API_KEY = os.getenv(
+    "OPENROUTER_API_KEY"
+)
+
+OPENROUTER_URL = (
+    "https://openrouter.ai/api/v1/chat/completions"
+)
+
 
 def save_memory(key,value):
     memory, created = Memory.objects.update_or_create(
@@ -24,3 +36,56 @@ def delete_memory(key):
 
 def list_memories():
     return Memory.objects.all()
+
+
+def extract_memory(text):
+
+    headers = {
+        "Authorization":
+            f"Bearer {OPENROUTER_API_KEY}",
+        "Content-Type":
+            "application/json"
+    }
+
+    payload = {
+        "model":
+            "deepseek/deepseek-chat",
+
+        "messages":[
+            {
+                "role":"system",
+
+                "content":(
+                    "You extract personal memories. "
+                    "Return ONLY valid JSON.\n\n"
+                    "Format:\n"
+                    "{\n"
+                    '  "key":"...",\n'
+                    '  "value":"..."\n'
+                    "}\n\n"
+                    "No markdown. "
+                    "No explanation."
+                )
+            },
+            {
+                "role":"user",
+                "content":text
+            }
+        ]
+    }
+
+    response = requests.post(
+        OPENROUTER_URL,
+        headers=headers,
+        json=payload
+    )
+
+    data = response.json()
+
+    content = (
+        data["choices"][0]
+        ["message"]
+        ["content"]
+    )
+
+    return json.loads(content)
