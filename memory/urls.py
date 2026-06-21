7daf073db1e04cb01492147b56808fbcffa44a97
Ayush Pal <ayush777pal2004@gmail.com>
2026-06-21 11:07:44 +0530

    urls for contact created

--- memory/urls.py
+++ memory/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ForgetMemoryAPIView,GetMemoryApiView,SaveMemoryApiView,ProcessMemoryAPIView
+from .views import SaveContactAPIView,CallContactAPIView,ForgetMemoryAPIView,GetMemoryApiView,SaveMemoryApiView,ProcessMemoryAPIView
 
 urlpatterns=[
     path('save/',SaveMemoryApiView.as_view()),
     path('get/',GetMemoryApiView.as_view()),
     path('process/',ProcessMemoryAPIView.as_view()),
-    path('forget/', ForgetMemoryAPIView.as_view())
+    path('forget/', ForgetMemoryAPIView.as_view()),
+    path("contact/save/", SaveContactAPIView.as_view()),
+    path("contact/call/",CallContactAPIView.as_view())
 ]
